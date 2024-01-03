29a90a9b259148a1c9e26ead04c679d728f5237b
Tiago Guerra <57842819+tiagodsg1@users.noreply.github.com>
2024-01-03 11:21:37 -0300

    Update Smartphone.cs

--- DesafioDioSmarphone/Models/Smartphone.cs
+++ DesafioDioSmarphone/Models/Smartphone.cs
@@ -3,12 +3,10 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
