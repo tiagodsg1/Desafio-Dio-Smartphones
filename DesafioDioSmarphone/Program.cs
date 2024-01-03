023eb79a20bfe2b9a7c445df2281ad9c6137cf98
Tiago Guerra <57842819+tiagodsg1@users.noreply.github.com>
2024-01-03 11:23:10 -0300

    Update Program.cs

--- DesafioDioSmarphone/Program.cs
+++ DesafioDioSmarphone/Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+using System;
+using Models;
+
+class Program
+{
+    static void Main()
+    {
+        Celular celularIphone = new Iphone { Marca = "Apple", Modelo = "iPhone 12" };
+        Celular celularNokia = new Nokia { Marca = "Nokia", Modelo = "3310" };
+
+        celularIphone.FazerChamada("123456789");
+        celularIphone.EnviarMensagem("987654321", "Olá, estou falando de um Iphone?");
+
+        celularNokia.FazerChamada("555-1234");
+        celularNokia.EnviarMensagem("555-5678", "Olá, estou falando de um Nokia");
+
+        Console.ReadKey();
+    }
+}
